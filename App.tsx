069a67b3aff92d0093d86a86609dc89475f74a20
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-04-15 23:59:23 +0600

    add image

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {Image, ScrollView, Text, View} from 'react-native';
+import image from './src/assets/logo/logoWithoutBackground.png';
+
 function App(): React.JSX.Element {
   return (
     <View>
       <ScrollView>
         <View>
           <Text>Welcome to Secretline</Text>
+          <Image src={image} />
         </View>
       </ScrollView>
     </View>
