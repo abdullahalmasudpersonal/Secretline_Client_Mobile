f023beb4c0c70f40d7a6a924118419b2c1a560ca
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2025-05-30 01:23:35 +0600

    create chatroom header

--- src/screens/chat/ChatRoomScreen.tsx
+++ src/screens/chat/ChatRoomScreen.tsx
@@ -1,38 +1,61 @@
 import { StatusBar, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
-import { useRoute } from '@react-navigation/native';
-// import { useGetSingleChatUserQuery } from '../../redux/features/chat/chatApi';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useGetSingleChatUserQuery } from '../../redux/features/chat/chatApi';
 import { useGetProfileQuery } from '../../redux/features/profile/profileApi';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Image } from 'react-native';
+import { NavigationProp } from '../../types/navigation.types';
+import { ROUTES } from '../../constants/routes';
+import { TextInput } from 'react-native';
 
 const ChatRoomScreen = () => {
     const route = useRoute();
+    const navigation = useNavigation<NavigationProp<typeof ROUTES.CHAT_ROOM>>();
     const { roomId } = route.params as { roomId: string };
     const { data: profileData } = useGetProfileQuery({});
-    //   const profileData = data?.data || "";
-    // const { data: chatData } = useGetSingleChatUserQuery(roomId);
     const { name, profileImg } = profileData?.data || '';
-    console.log(profileData);
+    ////////////////////////////////
+    const [messages, setMessages] = useState<{ content: string; senderId: string }[]>([]);
+    const { data: chatData } = useGetSingleChatUserQuery(roomId);
+
+    // setMessages  সার্ভার থেকে মেসেজ লোড করুন
+    useEffect(() => {
+        if (chatData?.data) {
+            setMessages(chatData?.data);
+        }
+    }, [chatData]);
+
+    console.log(chatData);
 
 
     return (
-        < >
+        <>
             <StatusBar backgroundColor="#30333d" barStyle="light-content" />
             <View style={styles.container}>
-                <View>
+                <View style={styles.nameAImg}>
+                    <Icon name="arrow-back" size={25} color="white" onPress={() => navigation.navigate(ROUTES.CHAT_LIST)} />
                     <Image style={styles.chatImage} source={{ uri: profileImg }} />
-                    <Text>{name}</Text>
+                    <Text style={styles.username}>{name}</Text>
                 </View>
-                <Icon name="more-vert" size={30} color="white" />
+                <View style={styles.callAMore}>
+                    <Icon name="video-camera-front" size={25} color="white" />
+                    <Icon name="call" size={25} color="white" />
+                    <Icon name="more-vert" size={25} color="white" />
+                </View>
             </View>
-            {/* <ScrollView>
-                <View>
-                    <Text>ChatRoomScreen</Text>
-                    <Text>{roomId}</Text>
-                </View>
-            </ScrollView> */}
+            <ScrollView style={styles.chattingMessage}>
+                {messages?.map((msg, index) => (
+                    <View key={index}>
+                        <Text>{msg?.content}</Text>
+                    </View>
+                ))
+                }
+            </ScrollView>
+            <View style={styles.sendMessageView}>
+                <TextInput style={styles.messageInput} />
+            </View>
         </>
     );
 };
@@ -40,18 +63,56 @@ const ChatRoomScreen = () => {
 export default ChatRoomScreen;
 
 const styles = StyleSheet.create({
+    /////////////// chat room start //////////////
     container: {
         backgroundColor: '#30333d',
         height: 57,
-        padding: 6,
+        paddingVertical: 6,
+        paddingHorizontal: 8,
         flexDirection: 'row',
-        alignItems: 'center',
+        // alignItems: 'center',
         justifyContent: 'space-between',
     },
+    nameAImg: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // gap:15,
+        // backgroundColor: 'blue',
+        // justifyContent: 'space-between',
+    },
+    username: {
+        color: 'white',
+        fontWeight: 800,
+        fontSize: 15,
+        marginLeft: 8,
+    },
     chatImage: {
-        width: 50,
-        height: 50,
+        width: 42,
+        height: 42,
         borderRadius: 50,
+        marginLeft: 12,
+    },
+    callAMore: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        // backgroundColor: 'purple',
+        gap: 19,
+    },
+    /////////////// chat room end //////////////
+
+    /////////////////////////
+    chattingMessage: {
+        flex: 1,
+        backgroundColor: 'rgb(27, 26, 31)',
+    },
+    // ////////////////////////////
+    sendMessageView: {
+        backgroundColor: 'rgb(12,54,15)',
+        height:60,
+    },
+    messageInput:{
+        width:300,
+        backgroundColor:'rgb(66, 66, 71)',
     },
 });
 
